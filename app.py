11daddb2cf988df4677b69b70504ae2216b0468c
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2025-09-26 18:15:41 +0100

    testimonials route added

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from api import api_bp, load_recipes
 from flasgger import Swagger
 
 app = Flask(__name__)
-swagger = Swagger(app)
+swagger = Swagger(app, template_file='swagger.yml')
 
-app.register_blueprint(api_bp)
+testimonials = []
 
 
 @app.route("/")
@@ -24,6 +24,11 @@ def recipes_page():
     return render_template("recipies.html", recipes=recipes)
 
 
+@app.route("/testimonials")
+def testimonials_page():
+    return render_template("testimonials.html")
+
+
 # Detail page
 @app.route("/recipe/<int:recipe_id>")
 def recipe_detail(recipe_id):
@@ -40,6 +45,27 @@ def recipe_detail(recipe_id):
         return render_template("not_found.html")
 
     return render_template("recipe_detail.html", recipe=recipe)
+
+
+@app.route("/testimonial/add/", methods=["POST"])
+def add_testimonial():
+    data = request.get_json()
+    name = data.get("name")
+    feedback = data.get("feedback")
+
+    if not name or not feedback:
+        return jsonify({"error": "Name and feedback are required"}), 400
+
+    testimonial = {"name": name, "feedback": feedback}
+    testimonials.append(testimonial)
+
+    return jsonify({"message": "Testimonial added successfully", "testimonial": testimonial}), 201
+
+
+@app.route("/testimonial/all/", methods=["GET"])
+def get_all_testimonials():
+    return jsonify(testimonials), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
